4d07fd1c71fbdf5dbe612ac2138b9a85eec7031b
Indrajeet Satish Thakare <106312854+IndraT97@users.noreply.github.com>
2024-04-29 20:49:52 -0500

    Update 183 - Customers Who Never Ordered.sql

--- SQL_Leetcode_Questions/Medium/183 - Customers Who Never Ordered.sql
+++ SQL_Leetcode_Questions/Medium/183 - Customers Who Never Ordered.sql
@@ -1,7 +1,16 @@
-insert into customers values(1,"Joe"),(2,"Henry"),(3,"Sam"),(4,"Max");
-insert into orders values(1,3),(2,1);
-select * from customers;
-select * from Orders;
+insert into customers 
+  values(1,"Joe"),
+  (2,"Henry"),
+  (3,"Sam"),
+  (4,"Max");
 
-select name from customers where ID not in (select c.ID from customers c right join orders o on c.id = o.customerid);
+insert into orders values(1,3),
+  (2,1);
+
+select name from 
+customers 
+where ID not in (select c.ID 
+                  from customers c 
+                  right join orders o 
+                  on c.id = o.customerid);
 
