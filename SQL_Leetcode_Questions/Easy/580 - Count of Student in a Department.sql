b4205ec21b1a53d7c5792c8bc351392809e30859
Indrajeet Satish Thakare <106312854+IndraT97@users.noreply.github.com>
2024-04-29 20:46:58 -0500

    Update 580 - Count of Student in a Department.sql

--- SQL_Leetcode_Questions/Easy/580 - Count of Student in a Department.sql
+++ SQL_Leetcode_Questions/Easy/580 - Count of Student in a Department.sql
@@ -1,7 +1,15 @@
-insert into student values(1,"Jack","M",1),(2,"Jane","F",1),(3,"mark","M",2);
-insert into Department1 values(1,"Engineering"),(2,"Science"),(3,"Law");
+insert into student 
+  values(1,"Jack","M",1),
+  (2,"Jane","F",1),
+  (3,"mark","M",2);
 
-select * from Department1;
-select * from student;
+insert into Department1 
+  values(1,"Engineering"),
+  (2,"Science"),
+  (3,"Law");
 
-select dept_name,ifnull(count(student_id),0) from student s right join Department1 d using (Dept_id) group by dept_name;
+select dept_name,ifnull(count(student_id),0) 
+from student s 
+right join Department1 d 
+using (Dept_id) 
+group by dept_name;
