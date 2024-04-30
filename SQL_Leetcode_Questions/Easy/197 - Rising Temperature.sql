2306d8b3f080687bb2ed7905ad137aaaa8798052
Indrajeet Satish Thakare <106312854+IndraT97@users.noreply.github.com>
2024-04-29 20:43:37 -0500

    Update 197 - Rising Temperature.sql

--- SQL_Leetcode_Questions/Easy/197 - Rising Temperature.sql
+++ SQL_Leetcode_Questions/Easy/197 - Rising Temperature.sql
@@ -1,6 +1,14 @@
-insert into weather values(1,"2015-01-01",10),(2,"2015-01-02",25),(3,"2015-01-03",20),(4,"2015-01-04",30);
-select * from weather;
+insert into weather 
+values(1,"2015-01-01",10),
+  (2,"2015-01-02",25),
+  (3,"2015-01-03",20),
+  (4,"2015-01-04",30);
 
+--------------------
 
-select w2.id from weather w1 join weather w2 on datediff(w2.recorddate,w1.recorddate) = 1 and w2.temperature > w1.temperature; 
+select w2.id 
+from weather w1 
+join weather w2 
+on datediff(w2.recorddate,w1.recorddate) = 1 
+and w2.temperature > w1.temperature; 
 
