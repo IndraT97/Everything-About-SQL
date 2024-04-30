93c28a597ff40252cd482841d0d0110a38d495e7
Indrajeet Satish Thakare <106312854+IndraT97@users.noreply.github.com>
2024-04-29 20:38:10 -0500

    Update 181 - Employees Earning More Than Their Managers.sql

--- SQL_Leetcode_Questions/Easy/181 - Employees Earning More Than Their Managers.sql
+++ SQL_Leetcode_Questions/Easy/181 - Employees Earning More Than Their Managers.sql
@@ -1,1 +1,11 @@
-insert into Employee1 values (1,"joe",7000,"3"),(2,"henry",80000,4),(3,"Sam",60000,NULL),(4,"MAX",90000,NULL);
+insert into Employee1 
+values (1,"joe",7000,"3"),
+        (2,"henry",80000,4),
+        (3,"Sam",60000,NULL),
+        (4,"MAX",90000,NULL);
+
+----
+select distinct(a.Name) from t_181 a
+left join t_181 b
+on a.managerid = b.id
+where a.salary > b.salary;
