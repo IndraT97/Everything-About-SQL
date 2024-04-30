c72920aa27cd0fce8cc0b2d6ca2193a9b26b356b
Indrajeet Satish Thakare <106312854+IndraT97@users.noreply.github.com>
2024-04-29 20:59:02 -0500

    Update 177 - N'th Highest Salary.sql

--- SQL_Leetcode_Questions/Easy/177 - N'th Highest Salary.sql
+++ SQL_Leetcode_Questions/Easy/177 - N'th Highest Salary.sql
@@ -1,11 +1,27 @@
-insert into Employee values(1,100),(2,200),(3,300),(4,400),(5,1200),(8,1500);
--- 2nd heighest salary
-select min(salary) as SecondHeighestSalary from (select distinct(salary) from employee order by salary desc limit 2) as t1;
--- 
+insert into Employee 
+  values(1,100),
+  (2,200),
+  (3,300),
+  (4,400),
+  (5,1200),
+  (8,1500);
+
+-- 2nd heighest salary - Query
+
+select min(salary) as SecondHeighestSalary 
+  from (select distinct(salary) 
+        from employee 
+        order by salary desc limit 2) as t1;
+
+-- Nth_Highest_Salary - Stored Procedure 
+
 delimiter &&
 create procedure Nth_Highest_Salary(IN Sal int)
 begin
-select min(Salary) as Salary from (select distinct(Salary) from Employee order by salary desc limit Sal)as t1;
+select min(Salary) as Salary 
+  from (select distinct(Salary) 
+  from Employee 
+  order by salary desc limit Sal) as t1;
 end 
 && delimiter ;
 
