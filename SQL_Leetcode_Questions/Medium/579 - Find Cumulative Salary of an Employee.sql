9e3be9ab59707b0081ee1f42a2af27af081a6b1d
Indrajeet Satish Thakare <106312854+IndraT97@users.noreply.github.com>
2024-04-29 20:56:35 -0500

    Update 579 - Find Cumulative Salary of an Employee.sql

--- SQL_Leetcode_Questions/Medium/579 - Find Cumulative Salary of an Employee.sql
+++ SQL_Leetcode_Questions/Medium/579 - Find Cumulative Salary of an Employee.sql
@@ -1,3 +1,17 @@
-insert into employee4 values(1,1,20),(2,1,20),(1,2,30),(2,2,30),(3,2,40),(1,3,40),(3,3,60),(1,4,60),(3,4,70);
-select *,sum(salary) over(partition by id order by month) from employee4 where (id,month) not in (select id , max(month) from employee4 group by id);
+insert into employee4 
+  values(1,1,20),
+  (2,1,20),
+  (1,2,30),
+  (2,2,30),
+  (3,2,40),
+  (1,3,40),
+  (3,3,60),
+  (1,4,60),
+  (3,4,70);
+
+select *,sum(salary) over(partition by id order by month) 
+  from employee4 
+  where (id,month) not in (select id , max(month) 
+                           from employee4 
+                           group by id);
 
