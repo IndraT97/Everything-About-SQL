e3d545c56368c7c35ad0937495439d8c120b8a81
Indrajeet Satish Thakare <106312854+IndraT97@users.noreply.github.com>
2024-04-29 20:38:46 -0500

    Update 178 - Rank Scores.sql

--- SQL_Leetcode_Questions/Easy/178 - Rank Scores.sql
+++ SQL_Leetcode_Questions/Easy/178 - Rank Scores.sql
@@ -1,6 +1,15 @@
 use practise_set;
-insert into scores values(3.50),(3.65),(4.0),(3.85),(4.0),(3.65);
-select Score,Dense_rank() over(order by Score desc) as test_rank from scores;
+insert into scores 
+  values(3.50),
+  (3.65),
+  (4.0),
+  (3.85),
+  (4.0),
+  (3.65);
+
+
+select Score,Dense_rank() over(order by Score desc) as test_rank 
+from scores;
 
 
 
