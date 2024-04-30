13217f9a33280fcb9f366827ef2b9f3f8b83b3e0
Indrajeet Satish Thakare <106312854+IndraT97@users.noreply.github.com>
2024-04-29 20:48:26 -0500

    Update 180 - Consecutive Numbers.sql

--- SQL_Leetcode_Questions/Medium/180 - Consecutive Numbers.sql
+++ SQL_Leetcode_Questions/Medium/180 - Consecutive Numbers.sql
@@ -1,2 +1,13 @@
-insert into logs values(1,1),(2,1),(3,1),(4,2),(5,1),(6,2),(7,2);
-select * from logs;
+with CTE as (
+select *,
+Lag(num,2) over(order by id) as previous_number2,
+lag(num,1) over(order by id) as previous_number,
+Lead(num,1) over(order by id) as next_number,
+Lead(num,2) over(order by id) as next_number2
+from t_180)
+
+select distinct(num) 
+from CTE
+where (num = previous_number and num = previous_number2)
+or (num = previous_number and num = next_number)
+or (num = next_number and num = next_number2) ;
