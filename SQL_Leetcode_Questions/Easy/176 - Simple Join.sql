6d50e0a804c403d37062fb3426fa645972217115
Indrajeet Satish Thakare <106312854+IndraT97@users.noreply.github.com>
2024-04-29 20:57:15 -0500

    Update 176 - Simple Join.sql

--- SQL_Leetcode_Questions/Easy/176 - Simple Join.sql
+++ SQL_Leetcode_Questions/Easy/176 - Simple Join.sql
@@ -1,6 +1,4 @@
-create database practise_set;
-use practise_set;
-show tables;
-describe address;
-describe person;
-select p.FirstName, p.LastName,a.city,a.state from person p join address a using (PersonId);
+select p.FirstName, p.LastName,a.city,a.state 
+from person p 
+join address a 
+using (PersonId);
