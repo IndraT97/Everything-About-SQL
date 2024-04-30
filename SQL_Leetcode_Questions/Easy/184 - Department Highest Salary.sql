5f3ea25d2ae9ae1a5eb793cce81555ac54f9446e
Indrajeet Satish Thakare <106312854+IndraT97@users.noreply.github.com>
2024-04-29 20:41:17 -0500

    Update 184 - Department Highest Salary.sql

--- SQL_Leetcode_Questions/Easy/184 - Department Highest Salary.sql
+++ SQL_Leetcode_Questions/Easy/184 - Department Highest Salary.sql
@@ -1,9 +1,9 @@
-insert into employee2 values(1,"Joe",70000,1),(2,"Jim",90000,1),(3,"Henry",80000,2),(4,"Sam",60000,2),(5,"Max",90000,1);
-insert into dept values(1,"IT"),(2,"Sales");
-select * from employee2;
-select * from dept;
-select departmentId, max(salary) from employee2 group by departmentId;
-
+insert into employee2 
+        values(1,"Joe",70000,1),
+        (2,"Jim",90000,1),
+        (3,"Henry",80000,2),
+        (4,"Sam",60000,2),
+        (5,"Max",90000,1);
 
 SELECT dept.Name AS Department, Employee2.Name AS Employee, Salary
 FROM Employee2
