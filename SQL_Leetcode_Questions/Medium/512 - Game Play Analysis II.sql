e0afd2e1e5e001a69115688c8c0179a5a975c8eb
Indrajeet Satish Thakare <106312854+IndraT97@users.noreply.github.com>
2024-04-29 20:51:43 -0500

    Update 512 - Game Play Analysis II.sql

--- SQL_Leetcode_Questions/Medium/512 - Game Play Analysis II.sql
+++ SQL_Leetcode_Questions/Medium/512 - Game Play Analysis II.sql
@@ -9,9 +9,9 @@ FROM (select player_id,device_id,min(event_date)
 
 SELECT a.player_id, b.device_id
 FROM
-(SELECT player_id, MIN(event_date) AS event_date 
-FROM Activity
-GROUP BY player_id) a
-JOIN Activity b
-ON a.player_id = b.player_id 
-AND a.event_date = b.event_date;
+      (SELECT player_id, MIN(event_date) AS event_date 
+      FROM Activity
+      GROUP BY player_id) a
+            JOIN Activity b
+            ON a.player_id = b.player_id 
+            AND a.event_date = b.event_date;
